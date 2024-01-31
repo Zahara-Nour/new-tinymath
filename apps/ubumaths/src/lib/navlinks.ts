fb2a24784d9dff36d6ef163b14f9a8e5d4f00121
David Le Jolly <zahara.alnour@gmail.com>
2024-01-31 10:13:24 +0300

    update club-info

--- apps/ubumaths/src/lib/navlinks.ts
+++ apps/ubumaths/src/lib/navlinks.ts
@@ -1,19 +1,20 @@
-import type { Links } from '../types/type';
+import type { Links } from '../types/type'
 
 const links: Links = [
 	{
 		text: 'AutoMaths',
 		url: '/automaths',
-		tooltip: 'Travailler les automatismes'
+		tooltip: 'Travailler les automatismes',
 	},
 	{ text: 'Blog', url: '/blog', tooltip: 'Le blog' },
 	{
 		text: 'Histoire',
 		url: `/histoire`,
-		tooltip: 'Histoire des Maths'
+		tooltip: 'Histoire des Maths',
 	},
-	{ text: 'Jeux', url: '/jeux', tooltip: 'Jeux mathématiques' }
+	{ text: 'Jeux', url: '/jeux', tooltip: 'Jeux mathématiques' },
+	{ text: 'Club Info', url: '/club-info', tooltip: 'Club informatique' },
 	// {text:'Navadra', url:'/navadra', tooltip:''},
-];
+]
 
-export default links;
+export default links
---
+++ apps/ubumaths/src/routes/(app)/club-info/+page.svelte
@@ -0,0 +1,22 @@
+<div class="container mx-auto max-w-screen-md p-4">
+	<div class="mb-12 mt-4 flex items-center justify-start gap-8">
+		<img alt="logo club informatique" src="/images/logo-club-info2.jpg" class="max-w-48" />
+		<h2 class="h2">Club informatique du lycée Voltaire</h2>
+	</div>
+
+	<p>
+		Au club informatique, on apprend à créer un site internet en utilisant des technologies modernes
+		comme Svelte, qui est un framework JS permettant de gérer des composants UI :
+	</p>
+	<img src="/images/pile.png" class="max-h-82 m-auto my-4" />
+
+	<p>Le worflow choisi est le suivant :</p>
+	<img src="/images/workflow.png" class="max-h-82 m-auto my-4" />
+
+	<div class="flex justify-center">
+		<a
+			class="btn variant-filled-primary m-auto my-4 p-4 text-xl"
+			href="/club-info/creation-nouveau-projet">Création d'un nouveau projet</a
+		>
+	</div>
+</div>
