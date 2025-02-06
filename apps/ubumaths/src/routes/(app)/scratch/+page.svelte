7338a4ef2dbb71248caa3448fa844d28d2dcd948
David Le Jolly <zahara.alnour@gmail.com>
2025-02-06 12:44:16 +0300

    debut scratch

---
+++ apps/ubumaths/src/routes/(app)/scratch/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex h-full w-full flex-col items-center justify-center gap-8">
+	<h1 class="h1 mb-8">Verbe scractcher au subjonctif imparfait :</h1>
+	<h1 class="h2 mb-8">Je scratchasse</h1>
+	<h1 class="h2 mb-8">Tu scratchasses</h1>
+	<h1 class="h2 mb-8">Il/elle scratchasse</h1>
+	<h1 class="h2 mb-8">Nous scratchassions</h1>
+	<h1 class="h2 mb-8">Vous scratchassiez</h1>
+	<h1 class="h2 mb-8">Ils scratchassent</h1>
+</div>
