bf5bc645d8c8daa1e6d4849e42f085366bf2c541
David Le Jolly <zahara.alnour@gmail.com>
2025-09-02 09:26:25 +0300

    update domain name

--- apps/ubumaths/src/routes/(app)/password-recovery/+page.server.ts
+++ apps/ubumaths/src/routes/(app)/password-recovery/+page.server.ts
@@ -5,10 +5,9 @@ import type { Actions } from './$types'
 export const actions: Actions = {
 	recovery: async ({ request, locals }) => {
 		const body = Object.fromEntries(await request.formData())
-		const { error: err } = await locals.supabase.auth.resetPasswordForEmail(
-			body.email as string,
-			{ redirectTo: 'https://ubumaths.net/update-password' },
-		)
+		const { error: err } = await locals.supabase.auth.resetPasswordForEmail(body.email as string, {
+			redirectTo: 'https://ubumaths.fr/update-password',
+		})
 		if (err) {
 			console.log(err.message)
 			return fail(500, { error: err.message })
